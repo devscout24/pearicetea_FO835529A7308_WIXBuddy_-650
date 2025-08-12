d751ffe5edab59b377ade45779a0cb429489d4f6
Niamulhuda-Nahid <niamulhuda6046@gmail.com>
2025-08-13 04:10:25 +0600

    feat: enhance layout of Contact Us page for better responsiveness and alignment

--- src/pages/ContuctUs/index.tsx
+++ src/pages/ContuctUs/index.tsx
@@ -9,12 +9,12 @@ export default function ContactUs() {
             <h1 className="text-3xl md:text-4xl font-semibold text-title02 text-center md:text-left mb-7">
                 Contact Us
             </h1>
-            <div className="flex items-center justify-between gap-5">
-                <div className="w-1/2">
+            <div className="flex flex-col md:flex-row items-center justify-center md:justify-between gap-5">
+                <div className="w-full md:w-1/2 text-center">
                     <label className="text-lg font-semibold">Address</label>
                     <p className="text-sm font-normal">{basicData.address}</p>
                 </div>
-                <div className="w-1/2">
+                <div className="w-full md:w-1/2 text-center">
                     <label htmlFor="" className="text-lg font-semibold">Message</label>
                     <p className="text-sm font-normal">{basicData.email}</p>
                     <p className="text-sm font-normal">{basicData.phone_code} {basicData.phone_number}</p>
